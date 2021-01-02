639df95e1c76838780f4ef3dfa01d883e960af9d
eliuabdiel <2018030333@upsin.edu.mx>
2021-01-02 11:56:24 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container,Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid="xl">
+      <Row>
+        <Col xl="2">
+          
+        </Col>
+        <Col xl="8">
+          <h1 className="text-center">Hola</h1>
+        </Col>
+        <Col xl="2">
+          
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
