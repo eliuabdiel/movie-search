37926262619156936e3bbfab44a2119d68565a86
eliuabdiel <2018030333@upsin.edu.mx>
2021-01-10 23:02:00 -0700

    Added Card format to the movies with the Poster image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Title }  from './components/Title'
 import { SearchForm }  from './components/SearchForm'
 import { Container,Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Movie } from './components/Movie';
 
 class App extends Component {
   state = { results: []}
@@ -14,29 +15,37 @@ class App extends Component {
   _renderResults () {
     const { results } = this.state
     return results.map(movie => {
-      return <p className="text-center" key={movie.imdbID}>{movie.Title}</p>
+      console.log(movie)
+      return (
+              <Col xl="4" lg="4" md="5" xs="10" className="mt-3 d-flex justify-content-center" key={movie.imdbID}>
+                <Movie 
+                  className="text-center" 
+                  title={movie.Title}
+                  year={movie.Year}
+                  poster={movie.Poster}
+                  />
+              </Col>
+                )
     })
   }
   render(){
     return (
       <Container fluid="xl">
-        <Row className="">
-          <Col xl="2">
-            
-          </Col>
-          <Col xl="8">
-          <Title styleName="mt-3">Search Movies</Title> {/*styleName are name of css classes, this will be sent as a prop*/}
+        <Row className="justify-content-center">
+          
+          <Col xl="8" lg="8" md="10" xs="12">
+            <Title styleName="mt-3">Search Movies</Title> {/*styleName are name of css classes, this will be sent as a prop*/}
             <SearchForm onResults={this._handleResults}/>
-            
+          </Col>
+        
+        </Row>
+        <Row className="justify-content-center">
+          
               {this.state.results.length === 0
                 ? <p className="text-center">Sin resultado</p>
                 : this._renderResults()
               }
-            
-          </Col>
-          <Col xl="2">
-            
-          </Col>
+          
         </Row>
       </Container>
     );
---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Card } from 'react-bootstrap'
+
+export class Movie extends Component{
+    static propTypes = {
+        title: PropTypes.string,
+        year: PropTypes.string,
+        poster: PropTypes.string
+    }
+    render(){
+        const { poster, title, year } = this.props
+
+        return(
+            <Card style={{ width: '18rem' }} className="">
+                <Card.Img variant="top" src={`${poster}`}/>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{year}</Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
