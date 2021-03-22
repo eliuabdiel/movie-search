7ea6c0d08372190dd2045e7cb4fcec668c0067e7
eliuabdiel <2018030333@upsin.edu.mx>
2021-03-22 01:00:25 -0700

    changed the API URL to https to avoid mixed content

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react'
-const API_KEY = 'fb2fa295'
+import React, { Component } from "react";
+const API_KEY = "fb2fa295";
 export class SearchForm extends Component {
-    state =  {
-        inputMovie: ''
-    }
-    _handleChange = (e) => {
-        this.setState({ inputMovie: e.target.value})
-    }
-    _handleSubmit = (e) => {
-        e.preventDefault()
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${this.state.inputMovie}`)
-            .then(res => res.json())
-            .then(results => {
-                const {Search = []} = results
-                this.props.onResults(Search)
-                
-            })
-    }
-    render(){
-        return(
-            <form onSubmit={this._handleSubmit}>
-                <div className="input-group"> 
-                    <input 
-                        onChange={this._handleChange}
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Movie to search"
-                    />
-                    <div className="input-group-append">
-                        <button className="btn btn-primary">
-                            <i className="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
-        )
-    }
+  state = {
+    inputMovie: "",
+  };
+  _handleChange = (e) => {
+    this.setState({ inputMovie: e.target.value });
+  };
+  _handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${this.state.inputMovie}`
+    )
+      .then((res) => res.json())
+      .then((results) => {
+        const { Search = [] } = results;
+        this.props.onResults(Search);
+      });
+  };
+  render() {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <div className="input-group">
+          <input
+            onChange={this._handleChange}
+            type="text"
+            className="form-control"
+            placeholder="Movie to search"
+          />
+          <div className="input-group-append">
+            <button className="btn btn-primary">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
